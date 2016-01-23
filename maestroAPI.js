5b929e7f592997a736c996e75b39ecb3ff14b825
Cyril Cohen <cohen@crans.org>
2016-01-23 02:06:48 +0100

    commiting new interface files

---
+++ maestroAPI.js
@@ -0,0 +1,319 @@
+"use strict";
+
+//Public variables
+var minPlayer = 1;
+var maxPlayer = 14;
+
+//max number of points an impro can receive
+var maxScorePerPlay = 5;
+
+//small Jquery extension to store an edition function
+$.editionCallback = [];
+$.editionNextId = 0;
+$.fn.extend({
+  editable: function (mode, callback) {
+    var thisId = $.editionNextId++;
+    this.addClass("editable_" + mode);
+    this.attr("edition_" + mode, thisId);
+    $.editionCallback[thisId] = callback;
+  },
+  isEditable: function(mode){
+    return this.attr("edition_" + mode);
+  },
+  edit: function (mode) {
+    var n = this.isEditable(mode);
+    if (n != undefined){
+      return $.editionCallback[n].apply(this,arguments)
+    }
+  }
+})
+
+$.activateEdition = function (mode, activate) {
+  $("body").addClass("edition_" + mode);
+  if (activate == undefined || activate) {
+    $("body").on("click",".editable_" + mode,
+		 function(){$(this).edit(mode)});
+  } else {
+    $("body").removeClass("edition_" + mode);
+    $("body").off("click",".editable_" + mode);
+  }
+  };
+
+$.deactivateEdition = function (mode) {
+  $.activateEdition(mode, false);
+}
+
+
+// jquery extension to edit using inputs in place
+$.fn.extend({
+  input : function(type, submit, cancel, tune){
+    var that = this.clone(true);
+    var input = $("<input type='" + type + "' value='" + that.text() + "'>")
+	.attr("class", that.attr("class"));
+    // extra transformation, user provided
+    if (tune) tune.call(this);
+    this.replaceWith(input);
+    input.focus();
+    input.keydown(function(event) {
+      if (event.which == 13) {
+	$(this).replaceWith(that);
+	submit(input.val());
+      }else if (event.which == 27) {
+	$(this).replaceWith(that);
+	cancel();
+      }
+    })
+  }
+});
+
+//small Jquery extension to recover player
+$.fn.extend({
+  player: function() {
+    return Player.byId(this.closest("li").attr("player"))
+  }
+})
+
+
+///////////////////////////////////////
+// Player class creation and methods //
+///////////////////////////////////////
+class Player {
+  constructor(num, name) {
+    // internal initialization
+    Player.data.list.push(this);
+    this.id = Player.data.id++;
+    this._eliminated = false;
+    this._selected = false;
+    this._score = [];
+    this._name = name;
+    this._num = num;
+
+    // initialization and refresh of the display
+    this.init().refresh();
+  }
+
+  set name(name){this._name = name; this.refresh();}
+  get name(){return this._name}
+
+  set num(num){this._num = num; this.refresh();}
+  get num(){return this._num;}
+
+  set eliminated(eliminated){
+    this._eliminated = eliminated;
+    this.unselect().refresh();
+  }
+  get eliminated(){return this._eliminated}
+  eliminate() {this.eliminated = true; return this}
+
+  set selected(selected){
+    if (this.eliminated) return undefined;
+    if (this._selected < selected) Player.data.selected++;
+    if (this._selected > selected) Player.data.selected--;
+    this._selected = selected;
+    this.refresh();
+  }
+  get selected() {return this._selected;}
+  select() {this.selected = true; return this}
+  unselect() {this.selected = false; return this}
+  toggle() {
+    if (this.selected) {this.unselect()} else {this.select()};
+    return this.selected;
+  }
+
+  set scoreList(score) {this._score = score; this.refresh();}
+  get scoreList() {return this._score;}
+  score() {return this.scoreList.reduce(((m, n) => m + n),0);}
+  scorePush(k) {this.scoreList = this._score.concat([k])}
+  nextRound() {return this.scoreList.length + 1}
+
+  refresh(){
+    // Store current state;
+    Player.store();
+    // Printing Num and Name and setting tags
+    this.$.find(".playerNum").text(this.num);
+    this.$.find(".playerName").text(this.name);
+    this.$.toggleClass("selected", this.selected);
+    this.$.toggleClass("eliminated", this.eliminated);
+    // Displaying score
+    var point = 0;
+    var currentBlock = undefined;
+    var $score = this.$.find(".playerScore").html("");
+    this._score = this._score.filter(n => 
+      (0 < n) && (n <= maxScorePerPlay));
+    $.each(this.scoreList, (i, score_i) => {
+      for(var j = 0; j < score_i; j++){
+	if (point % 5 == 0) {
+	  if (currentBlock) {currentBlock.addClass('complete')};
+	  currentBlock = $('<div class="scoreBlock block_' + point % 5 + '">').
+	    appendTo($score)
+	}
+	point++;
+	$('<div class="point score_' + i + ' point_' + point + '">')
+	  .appendTo(currentBlock)
+	  .addClass(i % 2 == 0 ? "even" : "odd")
+	  .editable("unsafe", function(){
+	    var player = this.player();
+	    this.input(
+	      "text",
+	      function(val){
+		player.scoreList[i] = Number(val);
+		player.refresh();
+	      },
+	      function(){player.refresh()},
+	      function(){this.attr("size","1")}
+	    );
+	    $score.find("div.score_" + i).remove();
+	  });
+      }
+    });
+    $score.append($('<div class="nextPoint">'));
+    // if no one is selected put class wait on the ul
+    // return this for chaining purpose
+    Player.HUDRefresh();
+    return this;
+  }
+
+  // init (re)creates the html supporting the display of the player
+  init(){
+    this.detach();
+    if (!(this.$ instanceof jQuery)) {
+      this.$ = $("<li>").addClass("playerLine").attr("player",this.id);
+      $("<div class='playerControl'>").appendTo(this.$).editable("unsafe", function(){
+	this.player().delete();
+      });
+      $("<div class='playerStatus'>").appendTo(this.$);
+      $("<div class='playerNum'>").appendTo(this.$).editable("unsafe", function(){
+	var player = this.player();
+	this.input("text",
+		   function(val){
+		     if (Number(val) != NaN) {player.num = Number(val)};
+		     player.refresh();
+		   },
+		   function(){player.refresh()})
+      });
+      $("<div class='playerName'>").appendTo(this.$).editable("unsafe", function(){
+	var player = this.player();
+	this.input("text",
+		   function(val){player.name = val; player.refresh()},
+		   function(){player.refresh()})
+      });
+      $("<div class='playerScore'>").appendTo(this.$);
+    }
+    $("#playerList").append(this.$);
+    return this;
+  }
+
+  remove() {if (this.$ instanceof jQuery) this.$.remove();}
+  detach() {if (this.$ instanceof jQuery) this.$.detach();}
+
+  delete() {
+    this.remove();
+    var index = Player.data.list.indexOf(this);
+    if (index != -1) Player.data.list.pop(index);
+    Player.store();
+    delete(this);
+  };
+}
+
+////////////////////////////////////////////////////////////////////
+// Global manipulation on all Players is handled by class methods //
+////////////////////////////////////////////////////////////////////
+Player.default_data = () => ({list: [], id: 0, selected: 0, round: 1});
+Player.data = Player.default_data();
+
+Player.HUDRefresh = () => {
+  // Updating round
+  $('.roundNumber').html(Player.data.round);
+  $('.nextRound').toggle(Player.nextRound() != Player.data.round);
+  // Updating global player statuses
+  $("#playerList").toggleClass("waiting", !Player.data.selected);
+  return Player;
+}
+
+Player.__defineGetter__("round", () => Player.data.round)
+Player.__defineSetter__("round", (r) => {
+  Player.data.round = r;
+  Player.store();
+  Player.HUDRefresh();
+})
+
+Player.selected = function () {
+  return Player.data.list.filter((p) => p.selected);
+}
+
+// refresh rebuilds the whole player list, sorting them according to
+// an order
+Player.refresh = function (orderList) {
+  //  updating players
+  var orders = {};
+  if (orderList) {
+    if (orderList instanceof Array) $.each(orderList, (i, order) => orders[order] = true);
+    else orders = orderList;
+  };
+  var lastOrders = Player.lastOrders ? Player.lastOrders : {};
+  var orderList = [];
+  for (var order in orders) {
+    if ((lastOrders[order] && orders[order] == "change"
+	&& lastOrders[order] != "rev")
+  	|| (orders[order] == "rev")) {
+      if (lastOrders[order] == "change") {orders[order] = "rev"};
+      orderList.push(revOrder(playerOrder[order]));
+    } else {
+      orderList.push(playerOrder[order]) ;
+    }
+  };
+  Player.lastOrders = orders;
+  Player.data.list.sort(lexOrder.call(this, orderList));
+  $.each(Player.data.list, (i, p) => {
+    p.__proto__ = Player.prototype;
+    p.init().refresh();
+  });
+  Player.HUDRefresh();
+  return Player;
+}
+
+// different available sorting orders
+var playerOrder = {
+  num : (y, x) => x.num < y.num,
+  name : (y, x) => x.name < y.name,
+  eliminated : (y, x) => x.eliminated < y.eliminated,
+  selected : (x, y) => x.selected < y.selected,
+  score : (x, y) => x.score() < y.score()
+}
+
+//composing them
+function lexOrder (l) {
+  return (y, x) => {
+    for (var i in l) {
+      if      (l[i](y,x)) return true;
+      else if (l[i](x,y)) return false};
+    return false}
+};
+
+var revOrder = (o) => ((x, y) => o(y, x));
+
+//We use local storage to survive page reload
+Player.store = function () {
+  localStorage.setItem('playerData', JSON.stringify(Player.data));
+};
+
+Player.restore = function () {
+  var playerData = JSON.parse(localStorage.getItem('playerData'));
+  Player.data = $.extend(Player.data, playerData);
+  Player.refresh();
+};
+
+Player.reset = function () {
+  $.each(Player.data.list, (i, p) => p.delete());
+  Player.data = Player.default_data();
+  Player.store();
+};
+
+Player.nextRound = () => 
+  Player.data.list.filter(p => !p.eliminated).reduce((m, p) =>
+    Math.min(m, p.nextRound ? p.nextRound() : Infinity), Infinity);
+
+Player.byPosition = (i) => Player.data.list[i];
+Player.byId = (i) => Player.data.list.filter((p) => (i == p.id))[0];
+Player.byNum = (i) => Player.data.list.filter((p) => (i == p.num))[0];
+Player.byName = (n) => Player.data.list.filter((p) => (n == p.name))[0];
---
+++ minimalInterface.js
@@ -0,0 +1,470 @@
+"use strict";
+
+$(() => {
+  ///////////////////////////////////////
+  //some common functionalities used throughout the app
+  ///////////////////////////////////////
+  
+  Player.restore();
+
+  $(".playerNum.header").click(() => Player.refresh({num : "change"}));
+  $(".playerName.header").click(() => Player.refresh({name : "change"}));
+  $(".playerScore.header").click(() => Player
+    .refresh({eliminated: true, score: "change"}));
+
+  $(".nextRound").button().click(() => Player.round = Player.nextRound());
+
+  $(".roundNumber").editable("unsafe", function (){
+    this.input("text",
+	       function(val){
+		 if (Number(val) != NaN) {Player.round = Number(val)};
+		 HUD.refresh();
+	       },
+	       function(){HUD.refresh()})
+  });
+
+  $('#lock').editable("safe", function () {
+    $.deactivateEdition("safe");
+    $.activateEdition("unsafe");
+  });
+
+  $('#lock').editable("unsafe", function () {
+    $.deactivateEdition("unsafe");
+    $.activateEdition("safe");
+  });
+
+  $(".playerControl.header").click(function(){
+    new Player(0,"Player");
+  })
+
+  $("#dialogScore").dialog({
+    autoOpen: false,
+    height: 200,
+    width: 300,
+    title: "Give a score to the players",
+    modal: true,
+    buttons: {
+      Give: function(){
+	var n = Number($("#newScore").val());
+	if (n < 1 || n > maxScorePerPlay) {
+	  dialogError("Invalid Score");
+	} else {
+	  console.log(n)
+	  $.each(Player.selected(), (i, p) => p.scorePush(n));
+	  $("#dialogScore").dialog( "close");
+	  $.each(Player.selected(), (i, p) => p.unselect());
+	}
+      },
+      Cancel: function() {
+	$("#dialogScore").dialog( "close" );
+      }
+    }
+  });
+
+  $.activateEdition("safe");
+  $("#lock").button();
+
+  $("#dialogError").dialog({
+    autoOpen: false,
+    // height: 300,
+    width: 400,
+    modal: true,
+    buttons: {
+      Cancel: function() {
+	$("#dialogError").dialog( "close" );
+      }
+    }
+  }).parent().addClass("ui-state-error");
+
+  var dialogError = (text) => $("#dialogError").text(text).dialog("open");
+
+  $('#giveScore').button().click(() => {
+    if (Player.selected().length > 0) {
+      $("#dialogScore").dialog("open")
+    }else{
+      dialogError("No player selected");
+    }
+  });
+
+  $(".playerStatus").editable("play", function () {
+    this.player().toggle()
+  });
+
+  $(".playerStatus").editable("eliminate", function () {
+    var pl = this.player();
+    pl.eliminated = !pl.eliminated;
+  });
+
+  $("#eliminatePlayers").button().editable("play", function(){
+    $.deactivateEdition("play");
+    $.activateEdition("eliminate");
+    $("#eliminatePlayers").button("option","label","Stop elimination");
+  });
+  $("#eliminatePlayers").button().editable("eliminate", function(){
+    $.deactivateEdition("eliminate");
+    $.activateEdition("play");
+    $("#eliminatePlayers").button("option","label","Begin elimination");
+  });
+  $.activateEdition("play");
+  
+  // $('dialog')
+  
+  // $('giveScore').on("click", function(){
+    
+});
+
+// // $(".roundNumber").editable(function(){
+//   //   this.input(function(){},function(){},)
+//   // });
+
+//   //the functions on the left
+//   $('#functionsListWrapper').click(() => {
+//     $('#functionsList').toggle();
+    
+//     if(currentPhase == 'wait') {
+//       $('#btn_selectPlayers').toggle(currentPhase == 'wait');
+//       $('#btn_attributeScore').toggle(currentPhase == 'wait');
+//       $('#btn_eliminatePlayers').toggle();
+//     } else if(currentPhase == 'playing') {
+//       $('#btn_selectPlayers').hide();
+//       $('#btn_attributeScore').show();
+//       $('#btn_eliminatePlayers').hide();
+//     }
+//   })
+
+//     //increment round num
+//     function incrementRoundNum() {
+// 	roundNum++;
+// 	$('#title .roundNumber').html(roundNum);
+//     }
+
+//     //the main function responsible for drawing the players list
+//     function drawplayerList() {
+//     	//console.log('drawing');
+
+//     	//we empty the display
+//     	$('#playerList').html('');
+//     	//We loop the diferent status in the order of display
+//     	for(statusNum in playersDataStatusList) {
+//     	    var status = playersDataStatusList[statusNum];
+
+//     	    //We loop the list of users
+//     	    for(var num in playersData) {
+
+//     		var playerData = playersData[num];
+
+
+//     		//if not correct status, we continue
+//     		if(playerData.status != status) continue;
+
+    		
+
+//     		$('#playersList').append('<li id="player'+num+'" playerid="'+num+'" class="playerLine '+status+'"><div class="playerName">'+num+'. '+playerData.name+'</div><div class="playerScore"></div></li>');
+		
+//     		//score display
+//     		for(var round in playerData.scores) {
+//     		    var points = playerData.scores[round];
+//     		    $('#player'+num+' .playerScore').append('<div class="improScore '+round+'" roundnum="'+round+'"></div>');
+//     		    for(var i=1;i<=maxScorePerPlay;i++) {
+//     			if(points >=i) $('#player'+num+' .playerScore .'+round).append('<div  pointnum="'+i+'" class="pointIcon point"></div>');
+//     			else $('#player'+num+' .playerScore .'+round).append('<div  pointnum="'+i+'" class="pointIcon noPoint"></div>');
+//     		    }
+//     		}
+//     	    } //end foreach player
+//     	} //end foreach status
+
+//     	//We save the users
+//     	localStorage.setItem('playersData', JSON.stringify(playersData));
+//     }
+
+//     $(document).on('click','#playerList .pointIcon',function(){
+//     	var playerId = $(this).closest('.playerLine').attr('playerid');
+//     	var score = $(this).attr('pointnum');
+//     	var round = $(this).closest('.improScore').attr('roundnum');
+
+//     	//console.log(playerId+' - '+score + ' - '+round);
+
+//     	playersData[playerId].scores[round] = parseInt(score);
+//     	drawplayerList();
+//     })
+
+
+//     ///////////////////////////////////////
+//     //first step, we ask the user names
+//     ///////////////////////////////////////
+
+//     //We create with javascript the field list
+//     for(var i=1;i<=maxPlayer;i++) {
+// 	$('#form_userNames form').append(' <div><label>Player '+i+'</label><input type="text" name="player'+i+'" playerid="'+i+'" class="text ui-widget-content ui-corner-all"></div>');
+//     }
+
+//     dialog_userNames = $( "#form_userNames" ).dialog({
+// 	autoOpen: false,
+// 	height: 600,
+// 	width: 350,
+// 	modal: true,
+// 	buttons: {
+//             Cancel: function() {
+// 		dialog_userNames.dialog( "close" );
+//             },
+//             "Let's play": startGame
+// 	}
+//     });
+
+//     if(playersData.length ==0) dialog_userNames.dialog( "open" );
+//     else drawplayerList();
+
+
+//     function startGame() {
+//     	var i = 1;
+//     	$( "#form_userNames input" ).each(function(){
+//     	    var userName = $(this).val();
+//     	    if(userName) {
+// 		//    			console.log(i+' '+userName);
+//     		playersData[i] = {
+//     		    'name':userName,
+//     		    'status':'active',
+//     		    'scores': {}
+//     		};
+//     		i++;
+//     	    }
+//     	})
+//     	    //not enought players
+//     	    if(i<=minPlayer) {
+//     		alert('minimum '+minPlayer+' players');
+//     		return;
+//     	    }
+//     	//We draw the players list
+//     	drawplayerList();
+
+//     	//it's ok, we can close the discussion
+//     	dialog_userNames.dialog( "close" );
+//     }
+
+
+
+
+
+
+
+
+
+//     ///////////////////////////////////////
+//     //second step, players selection
+//     ///////////////////////////////////////
+
+//     //We set the dialog box
+
+//     dialog_selectPlayers = $( "#form_userSelection" ).dialog({
+// 	autoOpen: false,
+// 	height: 600,
+// 	width: 350,
+// 	modal: true,
+// 	buttons: {
+//             Cancel: function() {
+// 		dialog_userNames.dialog( "close" );
+//             },
+//             "Let's play": savePlayersSelection
+// 	}
+//     });
+
+//     //The admin wants to select some players to play
+//     //We display the list of users still in the game
+//     $('#btn_selectPlayers').on('click',function() {
+
+// 	$('#form_userSelection form').html('');
+// 	//We loop the list of users
+// 	for(var num in playersData) {
+
+// 	    var playerData = playersData[num];
+
+// 	    //only active players can play
+// 	    if(playerData.status != 'active') continue;
+
+// 	    $('#form_userSelection form').append(' <div><input type="checkbox" playerid="'+num+'" id="userSelect_checkbox_'+num+'" /><label for="userSelect_checkbox_'+num+'">'+num+'. '+playerData.name+'</label></div>');
+
+// 	} //end foreach player
+
+// 	dialog_selectPlayers.dialog('open');
+//     })
+
+
+//     function savePlayersSelection() {
+
+//     	//We loop on the checkbox and save the playing ones
+//     	var atLeastOneSelected = false;
+//     	$( "#form_userSelection input" ).each(function(){
+//     	    var isPlaying = $(this).prop('checked');
+//     	    var userId = $(this).attr('playerid');
+//     	    if(isPlaying) {
+//     		playersData[userId].status = 'playing';
+//     		atLeastOneSelected = true;
+//     	    } else playersData[userId].status = 'notPlaying';
+//     	})
+
+//     	    if(!atLeastOneSelected) {
+//     		alert('You have to select at least one player');
+//     		return;
+//     	    }
+//     	//We draw the players list
+//     	drawplayerList();
+//     	//it's ok, we can close the discussion and update the status
+//     	dialog_selectPlayers.dialog( "close" );
+//     	$('#functionsListWrapper').trigger('click');
+//     	currentPhase = 'playing';
+//     }
+
+
+
+
+
+
+
+//     ///////////////////////////////////////
+//     //third step, score attribution
+//     ///////////////////////////////////////
+
+
+//     //We set the dialog box
+
+//     dialog_attributeScore = $( "#form_scoreAttribution" ).dialog({
+// 	autoOpen: false,
+// 	height: 220,
+// 	width: 350,
+// 	modal: true,
+// 	buttons: {
+//             Cancel: function() {
+// 		dialog_userNames.dialog( "close" );
+//             },
+//             "Save score": saveScore
+// 	}
+//     });
+
+//     //The admin wants to select some players to play
+//     //We display the list of users still in the game
+//     $('#btn_attributeScore').on('click',function() {
+// 	selectedPoints = 0;
+// 	$('#form_scoreAttribution form').html('<div class="improScore"></div>');
+// 	for(var i = 1; i <= maxScorePerPlay;i++) {
+// 	    $('#form_scoreAttribution form .improScore').append(' <div class="pointIcon noPoint" pointnum="'+i+'"></div>');
+// 	} //end foreach score
+
+// 	dialog_attributeScore.dialog('open');
+//     })
+
+//     $(document).on('click','#form_scoreAttribution form .improScore div',function() {
+// 	selectedPoints = parseInt($(this).attr('pointnum'));
+
+// 	for(var i = 1; i <= maxScorePerPlay;i++) {
+// 	    var pointElem = $('#form_scoreAttribution form .improScore div[pointnum='+i+']');
+// 	    pointElem.removeClass('noPoint');
+// 	    pointElem.removeClass('point');
+// 	    if(i <= selectedPoints) pointElem.addClass('point');
+// 	    else pointElem.addClass('noPoint');
+// 	}
+//     })
+
+//     function saveScore() {
+
+//     	if(selectedPoints == 0) {
+//     	    alert('You must select at least one point');
+//     	    return;
+//     	}
+//     	//We loop the list of users
+// 	for(var num in playersData) {
+
+// 	    var playerData = playersData[num];
+
+// 	    //only active players can play
+// 	    if(playerData.status == 'playing') {
+// 		playersData[num].scores['round'+roundNum] = selectedPoints;
+// 		playersData[num].status = 'active';
+// 	    }
+// 	    if(playerData.status == 'notPlaying') playersData[num].status = 'active';
+
+// 	} //end foreach player
+
+//     	//We draw the players list
+//     	drawplayerList();
+//     	//it's ok, we can close the discussion and update the status
+//     	dialog_attributeScore.dialog( "close" );
+//     	$('#functionsListWrapper').trigger('click');
+//     	currentPhase = 'wait';
+
+//     	incrementRoundNum();
+//     }
+
+
+
+//     ///////////////////////////////////////
+//     //forth step, players elimination
+//     ///////////////////////////////////////
+
+//     //We set the dialog box
+
+//     dialog_userElimination = $( "#form_userElimination" ).dialog({
+// 	autoOpen: false,
+// 	height: 600,
+// 	width: 350,
+// 	modal: true,
+// 	buttons: {
+//             Cancel: function() {
+// 		dialog_userNames.dialog( "close" );
+//             },
+//             "Eliminate": savePlayersEliminated
+// 	}
+//     });
+
+//     //The admin wants to select some players to play
+//     //We display the list of users still in the game
+//     $('#btn_eliminatePlayers').on('click',function() {
+
+// 	$('#form_userElimination form').html('');
+// 	//We loop the list of users
+// 	for(var num in playersData) {
+
+// 	    var playerData = playersData[num];
+
+// 	    //only active players can play
+// 	    if(playerData.status != 'active') continue;
+
+// 	    $('#form_userElimination form').append(' <div><input type="checkbox" playerid="'+num+'" id="userSelect_checkbox_'+num+'" /><label for="userSelect_checkbox_'+num+'">'+num+'. '+playerData.name+'</label></div>');
+
+// 	} //end foreach player
+
+// 	dialog_userElimination.dialog('open');
+//     })
+
+
+//     function savePlayersEliminated() {
+
+//     	//We loop on the checkbox and save the playing ones
+//     	$( "#form_userElimination input" ).each(function(){
+//     	    var isEliminated = $(this).prop('checked');
+//     	    var userId = $(this).attr('playerid');
+//     	    if(isEliminated) {
+//     		playersData[userId].status = 'eliminated';
+//     	    }
+//     	})
+
+//     	    //We draw the players list
+//     	    drawplayerList();
+//     	//it's ok, we can close the discussion and update the status
+//     	dialog_userElimination.dialog( "close" );
+//     	$('#functionsListWrapper').trigger('click');
+//     	currentPhase = 'wait';
+//     }
+
+//     ///////////////////////////////////////
+//     //fifth step, start new game
+//     ///////////////////////////////////////
+
+//     $('#btn_newGame').on('click',function() {
+//         Player.reset();
+// 	currentPhase = 'wait';
+// 	roundNum = 1;
+// 	dialog_userNames.dialog( "open" );
+// 	$('#functionsListWrapper').trigger('click');
+//     })
+
+// })
